833aec18463a64a53ea19923dd316361e32bffb5
itsahmadrezza <111778540+theahmadreza@users.noreply.github.com>
2023-01-05 22:07:10 +0330

    add searching feature to search for an username

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time, os
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
@@ -17,7 +18,8 @@ load_dotenv()
 # get variables from env file.
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
-
+# KEYWORD = {} -> to add usernames from json file right here\
+#                   and append it in keyword dict and make a loop for all usernames
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -50,15 +52,45 @@ login = WebDriverWait(driver, 10).until(
     EC.visibility_of_element_located(("css selector", "button[type='submit']"))
 ).click()
 
-
+time.sleep(7)
 First_Not_Now = WebDriverWait(driver, 10).until(
     EC.visibility_of_element_located((By.XPATH, "//button[contains(text(), 'Not Now')]"))
 ).click()
 
+time.sleep(5)
 # block notifications.
 Seccound_Not_Now = WebDriverWait(driver, 10).until(
     EC.visibility_of_element_located((By.XPATH, "//button[contains(text(), 'Not Now')]"))
 ).click()
+
+
+time.sleep(5)
+searchDIV = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(("css selector", "svg[aria-label='Search']"))
+).click()
+
+
+time.sleep(3)
+searchInput = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.CLASS_NAME, "_aauy"))
+)
+
+keyword = "nasa"
+
+time.sleep(5)
+searchInput.clear()
+
+
+time.sleep(5)
+searchInput.send_keys(keyword)
+
+# to send Enter key and search for it:
+time.sleep(5)
+Seccound_Not_Now = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, f"//div[contains(text(), '{keyword}')]"))
+).click()
+
+
 
 
 # TODO: fix the time for no sleep.
