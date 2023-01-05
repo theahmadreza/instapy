3ac24dd04ba1cf15c8a039a437c49e727a6fc638
itsahmadrezza <111778540+theahmadreza@users.noreply.github.com>
2023-01-06 02:13:24 +0330

    adding download feature
    in this commit the app can download an image until 4 times scrolling but for now it can't download reel and videos!

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
-import time, os
+import time, os, requests
 
 
 from selenium.webdriver.support import expected_conditions as EC
@@ -90,7 +90,63 @@ Seccound_Not_Now = WebDriverWait(driver, 10).until(
     EC.visibility_of_element_located((By.XPATH, f"//div[contains(text(), '{keyword}')]"))
 ).click()
 
+# scrolling the user page
 
+time.sleep(3)
+driver.execute_script(
+    "window.scrollTo(0, 4000);"
+)
+"""TODO:
+we should get scrolling 'y' position from there pages 
+"""
+
+# get images
+images = driver.find_elements(By.XPATH, "//img")
+
+images = [image.get_attribute('src') for image in images]
+
+# download images
+"""
+path = os.getcwd()
+
+path = os.path.join(path, keyword)
+
+os.mkdir(path)
+"""
+# fetching all posts.
+posts = []
+links = driver.find_elements(By.XPATH, "//a")
+
+for link in links:
+    post = link.get_attribute('href')
+    
+    if '/reel/' in post:
+        posts.append(post)
+
+print(posts)
+
+#  Download posts
+# TODO: counter is for name the images so i should to changes
+
+download_url = ''
+counter = 0
+
+for post in posts:
+    counter += 1
+    driver.get(post)
+    shortcode = driver.current_url.split("/")[-2]
+    time.sleep(7)
+
+    if driver.find_element("css selector", "img[style='object-fit: cover;']") is not None:
+        download_url = driver.find_element("css selector", "img[style='object-fit: cover;']").get_attribute('src')
+        r = requests.get(download_url)
+        with open(f"{keyword}-{str(counter)}.png", "wb") as h:
+            h.write(r.content)
+    else:
+        download_url = driver.find_element("css selector", "video[type='video/mp4']").get_attribute('src')
+        r = requests.get(download_url)
+        with open(f"{keyword}-{str(counter)}.mp4", "wb") as h:
+            h.write(r.content)
 
 
 # TODO: fix the time for no sleep.
